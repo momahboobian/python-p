34daa67c304e4607af0089f354ca710152a8a301
Mo Mahboobian <momahboobian@gmail.com>
2023-08-08 14:23:06 +0100

    Classes using STR and REPR

--- _D2/main_bin7_ex.py
+++ _D2/main_bin7_ex.py
@@ -1,12 +1,32 @@
 class Person:
     """Represents people using a name and age"""
-    def __init__(self):
+    def __init__(self, name, age=18):
         print('Person.__init__()')
+        self.name = name
+        self.age = age
+        self.address = 'London'
+
+    def __str__(self):
+        return f'Person {self.name} - {self.age}: {self.address}'
+
+    def __repr__(self):
+        return f'Person(name={self.name}, age={self.age} at={self.address})'
 
 
 print('Starting')
 
-p = Person()
-print(p)
+p = Person('John', age=21)
+print(p.name, p.age, p.address)
+p.name = 'Bob'
+print(p.name, p.age, p.address)
+
+b = Person('Melly', age=19)
+b.address = 'Manchester'
+print(b)
+
+
+data = [b, b]
+print(*data)
 
 print('Done', ("." * 30))
+
