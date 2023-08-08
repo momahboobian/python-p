073ce99e55ed716ee804f2f4727d50e9ea1717ab
Mo Mahboobian <momahboobian@gmail.com>
2023-08-08 09:52:15 +0100

    Adding main_bin5

---
+++ _D2/main_bin5.py
@@ -0,0 +1,13 @@
+def print_msg():
+    """Print out a hello world massage,
+    to use ut call:
+    print_msg"""
+    print('Hello world!')
+    print("-" * 30)
+
+
+print('Starting')
+
+print_msg()
+
+print('Done')
