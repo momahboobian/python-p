e4e85bcfd5c5ca9b5fffcd86928f2852815c9caa
Mo Mahboobian <momahboobian@gmail.com>
2023-08-08 10:10:34 +0100

    Naming function

--- _D2/main_bin5.py
+++ _D2/main_bin5.py
@@ -1,13 +1,31 @@
-def print_msg():
+def print_msg(massage):
     """Print out a hello world massage,
     to use ut call:
     print_msg"""
-    print('Hello world!')
-    print("-" * 30)
+    print(massage)
 
 
 print('Starting')
 
-print_msg()
+print_msg('Hellow word')
+print_msg('Good bye')
 
-print('Done')
+print('Done', ("-" * 30))
+
+def greeter(name,
+            title='Mx',
+            prompt='Welcome',
+            message='Hello'):
+    print(f'{prompt} {title} {name} -  {message}')
+
+
+print('Starting')
+greeter('John', 'Dr')
+greeter('Denise')
+greeter('Adam', message='Hi', title='Mr')
+greeter(title='Ms',
+        prompt='Hola',
+        name='Natalia',
+        message='Buenus Dias')
+print('Done', ("-" * 30))
+
