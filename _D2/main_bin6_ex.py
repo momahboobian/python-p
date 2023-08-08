3aeeb9b2359c10446b500dce1a5f6bc603393226
Mo Mahboobian <momahboobian@gmail.com>
2023-08-08 12:14:05 +0100

    Functional Style HOF approach

--- _D2/main_bin6_ex.py
+++ _D2/main_bin6_ex.py
@@ -21,10 +21,8 @@ print(a_global_var)
 print('Done', ("-" * 30))
 
 print('Starting')
-def f1(x): return x * x
-def f2(x, y): return x
-
-
+f1 = lambda x: x * x
+f2 = lambda x, y: x
 print(f1)
 print(f2)
 print(adder)
@@ -32,4 +30,52 @@ print('.' * 25)
 print(f1(5))
 print(adder(2, 3))
 print(f2(2, 3))
+print('Done', ("-" * 30))
+
+
+def is_even(item):
+    return item % 2 == 0
+
+
+def multplier(item):
+    return item * item
+
+
+print('Starting')
+
+data = [1, 3, 5, 6, 7, 9, 10]
+
+even_data = []
+for item in data:
+    if item % 2 == 0:
+        even_data.append(item)
+print(even_data)
+
+
+# Functional Style using a HoF
+even_data2 = list(filter(is_even, data))
+for d in even_data2:
+    print(d, ",", even_data2)
+
+even_data3 = list(filter(
+    lambda item: item % 2 == 0,
+    data))
+print(even_data3)
+
+
+
+
+result = []
+for item in data:
+    temp = item * item
+    result.append(temp)
+print(result)
+
+# Functional Style HOF approach
+results2 = list(map(lambda x: x * x, data))
+print(results2)
+
+results3 = list(map(multplier, data))
+print(results3)
+
 print('Done', ("-" * 30))
