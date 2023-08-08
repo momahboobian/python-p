4126c7bbdffba08295ac491c006e9661efe1ef7f
Mo Mahboobian <momahboobian@gmail.com>
2023-08-08 12:52:59 +0100

    Refactoring with nested map and filter

--- _D2/main_bin6.py
+++ _D2/main_bin6.py
@@ -1,3 +1,43 @@
+# Illustrates use of higher order functions such as filter, map and reduce
+from functools import reduce
+
+
+def average(data):
+    return sum(data) / len(data)
+
+
+def median(data):
+    sorted_data = sorted(data)
+    data_length = len(data)
+    index = (data_length - 1) // 2
+
+    if data_length % 2:
+        return sorted_data[index]
+    else:
+        return (sorted_data[index] +
+                sorted_data[index + 1]) / 2.0
+
+
+def minimum(data, index=0):
+    if index == 0:
+        data_slice = data
+    else:
+        data_slice = data[index:]
+    return min(data_slice)
+
+
+def maximum(data, index=0):
+    if index == 0:
+        data_slice = data
+    else:
+        data_slice = data[index:]
+    return max(data_slice)
+
+
+def data_range(data):
+    return minimum(data), maximum(data)
+
+
 def celsius_to_fahrenheit(celsius):
     return (celsius * 9 / 5) + 32
 
@@ -6,39 +46,39 @@ def fahrenheit_to_celsius(fahrenheit):
     return (fahrenheit - 32) * 5 / 9
 
 
-celsius_temperatures = [25.0, 30.5, 15.8, 20.2]
-fahrenheit_temperatures = []
+# Set up the data file
+readings = [13.5, 12.6, 15.3, 12.2, 16.6, 14.6, 15.6]
 
-print('Starting')
-# Convert all temperatures to Fahrenheit
-for celsius in celsius_temperatures:
-    fahrenheit = celsius_to_fahrenheit(celsius)
-    fahrenheit_temperatures.append(fahrenheit)
+print(f'Readings: {readings}')
 
+# Find minimum, maximum etc in readings
+print('Min temp in list =', minimum(readings))
+print('Max temp in list =', maximum(readings))
+print('Average temperature = {:.2f}'.format(average(readings)))
+print('Median temperature value =', median(readings))
+readings_range = data_range(readings)
+print(f'Range of temperatures from {readings_range[0]} to {readings_range[1]}')
+
+# Convert all the temperatures from Celsius to Fahrenheit
+fahrenheit_temperatures = list(map(celsius_to_fahrenheit, readings))
 print(f'Fahrenheit Temperatures: {fahrenheit_temperatures}')
 
-# Convert back to Celsius
-converted_celsius_temperatures = []
-for fahrenheit in fahrenheit_temperatures:
-    celsius = fahrenheit_to_celsius(fahrenheit)
-    converted_celsius_temperatures.append(celsius)
-
-
-print(f'Converted Celsius Temperatures: {converted_celsius_temperatures}')
-print('Done', ("-" * 30))
-
-
-# Select all temperatures above 14.0
-temperatures = [25.0, 30.5, 15.8, 20.2, 10.0, 18.7]
-higher_temperatures = [temp for temp in temperatures if temp > 14.0]
-
-print('Starting')
+# Find all temperatures above 14.0
+higher_temperatures = list(filter(lambda r: r > 14.0, readings))
 print(f'Temperatures above 14.0: {higher_temperatures}')
-print('Done', ("-" * 30))
 
-# Convert all the temperatures above 14.0 to Fahrenheit
-converted_temperatures = [celsius_to_fahrenheit(temp) for temp in higher_temperatures]
+# Total all the readings
+total_of_readings = sum(readings)
+print(f'Total of all readings = {total_of_readings}')
 
-print('Starting')
+# Total of all readings using reduce
+result = reduce(lambda total, value: total + value, readings)
+print(f'Total value of all readings is {result}')
+
+# Convert all readings above 14.0 to Fahrenheit
+converted_temperatures = list(map(celsius_to_fahrenheit, filter(lambda r: r > 15.5, readings)))
 print(f'Fahrenheit Temperatures above 14.0c: {converted_temperatures}')
+
+print('Done')
+
 print('Done', ("-" * 30))
