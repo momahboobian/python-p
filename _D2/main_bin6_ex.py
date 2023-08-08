2e173f1b29151f56a693fae805243d539aaa7325
Mo Mahboobian <momahboobian@gmail.com>
2023-08-08 11:55:40 +0100

    Lambda func

--- _D2/main_bin6_ex.py
+++ _D2/main_bin6_ex.py
@@ -19,3 +19,17 @@ print(a_global_var)
 print(do_something())
 print(a_global_var)
 print('Done', ("-" * 30))
+
+print('Starting')
+def f1(x): return x * x
+def f2(x, y): return x
+
+
+print(f1)
+print(f2)
+print(adder)
+print('.' * 25)
+print(f1(5))
+print(adder(2, 3))
+print(f2(2, 3))
+print('Done', ("-" * 30))
